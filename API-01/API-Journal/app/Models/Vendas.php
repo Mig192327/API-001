b8d7d97e4d25326aeb0dadbd01b6f5cbc0b1bc80
Mig192327 <miguel.samuel.mss@gmail.com>
2025-02-28 22:15:39 -0300

    T

---
+++ API-01/API-Journal/app/Models/Vendas.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vendas extends Model
+{
+    protected $fillable = [
+        'Nome_Prod',
+        'Descricao_Prod',
+        'Data_Venda',
+    ];
+}
